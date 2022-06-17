cbfcc06bafb5109fd2efbe85de3e6d78637ca586
Notsony <bukanSony@outlook.com>
2022-06-17 17:07:12 +0800

    commit 1

---
+++ prememo.php
@@ -0,0 +1,515 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+
+  <title>NOTNIV |Prememo|</title>
+  <link href='http://fonts.googleapis.com/css?family=Alfa+Slab+One' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="stylesheets/base.css">
+  <link rel="stylesheet" href="stylesheets/skeleton.css">
+  <link rel="stylesheet" href="stylesheets/layout.css">
+  <link rel="stylesheet" href="stylesheets/styles.css">
+  <link rel="stylesheet" href="stylesheets/all.css">
+  <link rel="stylesheet" href="stylesheets/main.css">
+  <link rel="stylesheet" href="stylesheets/text.css">
+  <link rel="stylesheet" href="css\bootstrap.min.css">
+
+  <link rel="stylesheet" href="css/text.css">
+  <link rel="stylesheet" href="css/all.css">
+  <link rel="stylesheet" href="css/main.css">
+
+    <style>
+	.navbar-collapse {
+	  text-align:center;
+	  padding-top:17px;
+	}
+
+	.navbar-nav {
+	  display:inline-block;
+	  float: none;
+	}
+  </style>
+ </head>
+<body>
+
+
+<!--Gambarnya bro!!-->
+<div class="relative header">
+	<img class="absolute header_image03" src="image/01_image02.png" border="0" />
+	<img class="absolute header_image02" src="image/01_image03.png" border="0" />
+	<!--<img class="absolute header_image01" src="image/01_image01.png" border="0" />-->
+</div>
+
+
+<!--Bagian Menunya-->
+
+
+<form class="container clearfix"><center>
+	<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+    <div  class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+	  <li class><a href="index.php">Home<span class="sr-only">(current)</span></a></li>
+      <li class><a href="chaos.php">Chaos<span class="sr-only">(current)</span></a></li>
+	  <li class><a href="zx.php">Zillion X Enemy<span class="sr-only">(current)</span></a></li>
+	  <li class="active"><a href="prememo.php">Precious Memory<span class="sr-only">(current)</span></a></li>
+	  <li class><a href="wixoss.php">Wixoss<span class="sr-only">(current)</span></a></li>
+	  <li class><a href="ll.php">Luck & Logic<span class="sr-only">(current)</span></a></li>
+       
+      </ul>
+
+		</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+
+	<input class="nav-menu" id="all" type="radio" name="filter"checked="checked"/>
+	<label for="all">All Expansion</label>
+
+    <input class="nav-menu" id="trials" type="radio"name="filter"/>
+	<label for="trials">Trial Deck</label>
+
+	<input class="nav-menu" id="boosters" type="radio"name="filter"/>
+	<label for="boosters">Booster Box</label>
+    
+	<input class="nav-menu" id="extras" type="radio" name="filter"/>
+	<label for="extras">Extra Box</label>
+    
+	<input class="nav-menu" id="translations" type="radio"name="filter"/>
+	<label for="translations">On Translation</label>
+  
+<section class="portfolio">
+	<figure class="four columns all trial" data-category="trial">
+		<img src="Prememo_D/01.jpg" alt="01">
+		
+		<footer><h4>Dog Days</h4></footer>
+			
+		
+	</figure>
+    
+    
+	<figure class="four columns all booster" data-category="booster">
+		<img src="Prememo_D/01_2.jpg" alt="01_2">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+    
+	<figure class="four columns all trial" data-category="trial">
+		<img src="Prememo_D/02.jpg" alt="02">
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+    
+	<figure class="four columns all trial" data-category="illustration">
+		<img src="Prememo_D/03.jpg" alt="03">
+			<h4>Lorem Ipsum</h4>
+
+
+	</figure>
+    
+	<figure class="four columns all trial" data-category="poster">
+		<img src="Prememo_D/04.jpg" alt="04">
+
+			<h4>Lorem Ipsum</h4>
+		
+
+	</figure>
+    
+	<figure class="four columns all trial" data-category="illustration">
+		<img src="Prememo_D/05.jpg" alt="05">
+
+			<h4>Lorem Ipsum</h4>
+	
+	</figure>
+    
+	<figure class="four columns all trial " data-category="typography">
+		<img src="Prememo_D/06.jpg" alt="06">
+		
+			<h4>Lorem Ipsum</h4>
+		
+	</figure>
+    
+	<figure class="four columns all trial" data-category="package">
+		<img src=" Prememo_D/07.jpg" alt="07">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+    
+	<figure class="four columns all trial" data-category="poster">
+		<img src="Prememo_D/08.jpg" alt="08">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+    
+	<figure class="four columns all trial " data-category="package">
+		<img src="Prememo_D/09.jpg" alt="09">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+    
+	<figure class="four columns all trial " data-category="illustration">
+		<img src="Prememo_D/10.jpg" alt="10">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+
+	<figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/11.jpg" alt="11">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/12.jpg" alt="12">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/13.jpg" alt="13">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/14.jpg" alt="14">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/15.jpg" alt="15">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/16.jpg" alt="16">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/16_2.jpg" alt="16_2">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/17.jpg" alt="17">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/18.jpg" alt="18">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/19.jpg" alt="19">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/20.jpg" alt="20">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/21.jpg" alt="21">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/22.jpg" alt="22">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/23.jpg" alt="23">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/24.jpg" alt="24">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/25.jpg" alt="25">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/26.jpg" alt="26">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/27.jpg" alt="27">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/28.jpg" alt="28">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/29.jpg" alt="29">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/30.jpg" alt="30">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+
+	<figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/31.jpg" alt="31">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/32.jpg" alt="32">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/32_2.jpg" alt="32_2">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/33.jpg" alt="33">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/34.jpg" alt="34">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/35.jpg" alt="35">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/36.jpg" alt="36">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/37.jpg" alt="37">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/37_2.jpg" alt="37_2">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/38.jpg" alt="38">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/38_2.jpg" alt="38_2">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/39.jpg" alt="39">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/40.jpg" alt="40">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/41.jpg" alt="41">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/42.jpg" alt="42">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/43.jpg" alt="43">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/44.jpg" alt="44">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/45.jpg" alt="45">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/46.jpg" alt="46">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/47.jpg" alt="47">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/48.jpg" alt="48">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/49.jpg" alt="49">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/50.jpg" alt="50">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/51.jpg" alt="51">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/52.jpg" alt="52">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/53.jpg" alt="53">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/54.jpg" alt="54">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/55.jpg" alt="55">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/56.jpg" alt="56">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/57.jpg" alt="57">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/58.jpg" alt="58">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+
+	<figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/59.jpg" alt="59">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+
+	<figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/60.jpg" alt="60">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/60_2.jpg" alt="60_2">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+
+	<figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/61.jpg" alt="61">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/62.jpg" alt="62">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/63.jpg" alt="63">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/63_2.jpg" alt="63_2">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/64.jpg" alt="64">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/65.jpg" alt="65">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure><figure class="four columns all illustration " data-category="illustration">
+		<img src="Prememo_D/66.jpg" alt="66">
+		
+			<h4>Lorem Ipsum</h4>
+			
+	</figure>
+
+</section>
+</center>
+</form>
+
+<script src="js/jquery-1.11.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<footer class="container clearfix footer">
+<div class="contact">
+<ul>
+	<li class="contact-name">Vinton</li>
+	<li class="contact-phone">(+628)78-392-5154-2</li>
+	<li class="contact-email">notnivProject@gmail.com</li>
+</ul>
+</div>
+
+<div class="social">
+	<p class="copyright">Copyright Notniv Project 2015</p>
+<ul>
+<li class="social-dribbble">
+	<a href="#">Dribbble</a></li>
+<li class="social-facebook">
+	<a href="#">Facebook</a></li>
+<li class="social-twitter">
+	<a href="#">Twitter</a></li>
+</ul>
+</div>
+
+</footer>
+
+</body>
+</html>
